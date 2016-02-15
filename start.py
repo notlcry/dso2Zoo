be0f4110c245c97258f2494ad8538bd608dbdf08
huiyu <huiyu@dadynet.com>
2016-02-15 13:41:10 +0800

    add time statis

--- start.py
+++ start.py
@@ -1,6 +1,7 @@
 # coding: utf-8
 __author__ = 'huiyu'
 
+from datetime import datetime
 import time
 from swagger_client.apis import account_api
 from swagger_client.apis import vpnclient_api
@@ -16,11 +17,10 @@ def full_sync():
         dso_vm_client = vm_api.VmApi()
 
         # get 1 page count 100 for now, must for page request long term
-        rtn = dso_account_client.accounts_get(1, 100)
+        rtn = dso_account_client.accounts_get(1, 500)
         accounts = rtn.results.item
 
-        client = zk_client.ZookClient("localhost:2181")
-
+        # client = zk_client.ZookClient("localhost:2181")
         # client.create_mapping_accounts(accounts.item)
 
         account_info_map = {}
@@ -40,6 +40,9 @@ def full_sync():
             # vm_info = dso_vm_client.vm_account_id_get(account.id)
             # client.gen_vm_path(vm_info)
 
+        start_sync_zoo = datetime.now()
+        # client = zk_client.ZookClient("10.74.113.102:2181")
+        client = zk_client.ZookClient("localhost:2181")
         client.delete_dso_path()
         client.create_accounts_path(accounts)
 
@@ -47,8 +50,11 @@ def full_sync():
             client.create_accountinfo_path(account_info_map[account.id])
             client.gen_mapping_pre_account(account_info_map[account.id], vpn_client_map[account.id])
 
+        client.commit()
         client.stopZooK()
-
+        end_sync_zoo = datetime.now()
+        print "sync zookeeper use: "
+        print (end_sync_zoo - start_sync_zoo)
     except Exception as exp:
         print exp.message
         traceback.print_exc()
@@ -56,5 +62,8 @@ def full_sync():
 
 if __name__ == "__main__":
     while True:
+        start = time.time()
         full_sync()
-        time.sleep(120)
+        used = time.time() - start
+        print "full sync finish, use %d sec " % used
+        time.sleep(60)
