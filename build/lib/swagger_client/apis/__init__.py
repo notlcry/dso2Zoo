6f6a328be92745c6e67d07bc1486a98058ad7eac
huiyu <huiyu@dadynet.com>
2016-02-23 19:58:30 +0800

    sync

--- build/lib/swagger_client/apis/__init__.py
+++
@@ -1,4 +0,0 @@
-from __future__ import absolute_import
-
-# import apis into api package
-from .estimates_api import EstimatesApi
--- build/lib/swagger_client/models/__init__.py
+++
@@ -1,5 +0,0 @@
-from __future__ import absolute_import
-
-# import models into model package
-from .error import Error
-from .account import Account
